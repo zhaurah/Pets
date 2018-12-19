27d402f6378bd51102f40f9d087bf3f844837610
Sara Vikström Edvinsson <sara.vikstrom.edvinsson@gmail.com>
2018-12-19 13:42:13 +0100

    Initial commit

---
+++ Pets/Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public string Sound { get; set; }
+        public bool CanFly { get; set; }
+
+        public string Show()
+        {
+            if (CanFly == true)
+            {
+                return String.Format("Name: {0}\nSound: {1}\nType: {2} and can fly!!", Name, Sound, GetType());
+            }
+            else
+            {
+                return String.Format("Name: {0}\nSound: {1}\nType: {2}", Name, Sound, GetType());
+            }
+        }
+    }
+
+    class Dog : Animal
+    {
+        public Dog(string name)
+        {
+            Name = name;
+            Sound = "Woff!";
+            CanFly = false;
+        }
+    }
+
+    class Cat : Animal
+    {
+        public Cat(string name)
+        {
+            Name = name;
+            Sound = "Mjau!";
+            CanFly = false;
+        }
+    }
+
+    class Bird : Animal
+    {
+        public Bird(string name)
+        {
+            Name = name;
+            Sound = "Twitter!";
+            CanFly = true;
+        }
+    }
+
+}
---
+++ Pets/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Program
+    {
+        static List<Animal> animals = new List<Animal>();
+        private static readonly Random getrandom = new Random();
+        
+        static void Main(string[] args)
+        {
+            
+            animals.Add(new Cat("Enpejst"));
+            animals.Add(new Dog("Hot"));
+            animals.Add(new Bird("Inamnamn"));
+            animals.Add(new Dog("Ilito"));
+
+            Console.WriteLine(@"--------------------
+
+Welcome....
+
+        GET A RANDOM ANIMAL!
+
+Press any key to continue... (or escape to exit)");
+
+            ConsoleKey ck = Console.ReadKey().Key;
+
+            while (ck != ConsoleKey.Escape)
+            {
+                int n = getrandom.Next(0, animals.Count - 1); // Creates a number between 0 and last index of the list
+
+                Console.WriteLine("\nYour animal is.... ");
+                Console.WriteLine(animals[n].Show());
+                Console.WriteLine("Press the Escape (Esc) key to quit or any key to continue: \n");
+                ck = Console.ReadKey().Key;
+            }
+
+
+        }
+    }
+}
